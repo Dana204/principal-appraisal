5f62d3ff77d1261f579847f5a56c1f45f8b03756
Dana Campbell <dana@dimepay.net>
2025-08-02 09:54:13 -0500

    updated home section image

--- .history/vite.config_20250802095308.js
+++ .history/vite.config_20250802095308.js
@@ -3,7 +3,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
-import path from 'path'
 
 export default defineConfig({
 	base: '/principal-appraisal/',
