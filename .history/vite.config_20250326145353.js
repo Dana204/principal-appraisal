1d61c84e524dfa0d81ce687abf0b8b0584a93fc6
Dana Campbell <dana@dimepay.net>
2025-08-02 09:51:01 -0500

    Configure GitHub Pages deployment

--- .history/vite.config_20250326145353.js
+++ .history/vite.config_20250326145353.js
@@ -5,6 +5,5 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-	base: '/principal-appraisal/',
 	plugins: [svgr(),react() ],
 });
